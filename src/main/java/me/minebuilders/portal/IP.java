97fa7eae0036a33059acd4eff22b669bdbfbec6c
julian <julien.mc1997@gmail.com>
2024-03-10 04:13:02 +0100

    Integrated LanguageManager and Enhancements to Plugin Initialization
    This commit introduces significant enhancements and new features to the IPortal plugin to improve user experience and developer friendliness. The `IP` main class has been updated to integrate multilingual support through the `LanguageManager` and to enhance logging upon plugin startup.
    
    Key Changes:
    
    - **Integration of LanguageManager**: The `LanguageManager` class has been added and integrated into the `onEnable` method of the `IP` class. This allows the plugin to support localized messages, facilitating adaptation to various languages.
    
    - **Enhanced Logging at Startup**: The `onEnable` method has been expanded to display an ASCII art logo and information about the plugin version and authors in the server log. This not only provides aesthetic enhancements but also clear information about the plugin at startup.
    
    - **Dynamic Plugin Initialization Notifications**: In addition to existing messages announcing the plugin's activation and deactivation, dynamic information about the plugin's version and contributors is now displayed in the console log. This promotes transparency and makes it easier to identify the plugin version and recognize the contributors.
    
    - **Listing of Authors**: An explicit listing of authors, including the original author, resource manager, and the developer who has been updating the plugin since version 2.0, has been added to acknowledge each contributor's effort.
    
    These changes not only enhance the functionality and usability of the plugin but also contribute to the maintenance of the plugin community by highlighting the significant contributions of developers and contributors.
    
    Implemented by _JuL1En_, this commit marks a significant milestone in the continued development of the IPortal plugin by laying a strong foundation for future expansions and improvements.

--- src/main/java/me/minebuilders/portal/IP.java
+++ src/main/java/me/minebuilders/portal/IP.java
@@ -5,6 +5,7 @@ import me.minebuilders.portal.listeners.PortalListener;
 import me.minebuilders.portal.listeners.WandListener;
 import me.minebuilders.portal.portals.Portal;
 import me.minebuilders.portal.storage.Data;
+import me.minebuilders.portal.storage.LanguageManager;
 import me.minebuilders.portal.tab.PortalTab;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
@@ -13,10 +14,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public final class IP extends JavaPlugin {
 
@@ -28,20 +26,51 @@ public final class IP extends JavaPlugin {
 
     public static Data data;
 
+    public static LanguageManager languageManager;
+
     public void onEnable() {
         instance = this;
+
+        getLogger().info("  _____                      _          _ ");
+        getLogger().info(" |_   _|                    | |        | |");
+        getLogger().info("   | |   _ __    ___   _ __ | |_  __ _ | |");
+        getLogger().info("   | |  | '_ \\  / _ \\ | '__|| __|/ _` || |");
+        getLogger().info("  _| |_ | |_) || (_) || |   | |_| (_| || |");
+        getLogger().info(" |_____|| .__/  \\___/ |_|    \\__|\\__,_||_|");
+        getLogger().info("        | |                               ");
+        getLogger().info("        |_|                               ");
+
+        String version = getDescription().getVersion();
+        List<String> authors = Arrays.asList("Original Author: bob7l"," Resource Manager: Spoopy"," Updated since V2.0: _JuL1En_");
+
+
+        Bukkit.getConsoleSender().sendMessage("[Iportal] §eV" + version + "§a coded by §e" + authors + " §ais initializing...");
+
         data = new Data(this);
+
+        // Added by _JuL1En_
+        // Initializes the LanguageManager with the current plugin instance (`this`).
+        // This allows the LanguageManager to access plugin resources and configurations,
+        // enabling it to load and manage localized messages for multilingual support.
+        languageManager = new LanguageManager(this);
+
         Bukkit.getMessenger().registerOutgoingPluginChannel((Plugin)this, "BungeeCord");
-        getCommand("iportal").setExecutor((CommandExecutor)new CommandListener());
+
+        // Added by _JuL1En_
+        // Assigns a TabCompleter to the "iportal" command, enabling automatic completion of command arguments,
+        // based on the current plugin instance for context-sensitive suggestions.
         getCommand("iportal").setTabCompleter((TabCompleter) new PortalTab(this));
+
         getServer().getPluginManager().registerEvents((Listener)new WandListener(this), (Plugin)this);
         getServer().getPluginManager().registerEvents((Listener)new PortalListener(this), (Plugin)this);
-        Util.log("IPortal has been enabled!");
+
+        Bukkit.getConsoleSender().sendMessage("[Iportal] §aPlugin has been enabled");
+
     }
 
     public void onDisable() {
         instance = null;
-        Util.log("IPortal has been disabled!");
+        Bukkit.getConsoleSender().sendMessage("[Iportal] §cPlugin has been disabled");
     }
 
     public static Data getData() {
